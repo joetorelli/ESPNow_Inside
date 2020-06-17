0c9f338a70f68f92506e5de74e4e3624a44f71a4
unknown <josephtorelli@gmail.com>
2020-06-17 12:56:38 -0500

    inside getting struct

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ void setup()
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
-  GetMacAdr();
+  //GetMacAdr();
 }
 void loop()
 {
