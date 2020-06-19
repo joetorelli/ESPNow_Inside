b342ef00118597a8a7df5942c8c9802d0553e1f8
Joe Torelli <josephtorelli@gmail.com>
2020-06-19 08:09:39 -0500

    adding oled display

---
+++ src/OLED.h
@@ -0,0 +1,29 @@
+#ifndef OLED_H
+
+#define OLED_H
+#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+//#include "RTClib.h"
+//#include "settings.h"
+//#include "sensor_readings.h"
+//#include <Adafruit_BME280.h>
+//#include "SRF.h"
+//#include "ezTime.h"
+
+/******************   OLED_Display  *******************/
+#define SCREEN_WIDTH 128  //  OLED_Display width, in pixels
+#define SCREEN_HEIGHT 128 //  OLED_Display height, in pixels
+
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+// void DisplaySensor(Adafruit_SSD1306 *Disp, BME_Sensor *SenVal);
+// void DisplaySwitches(Adafruit_SSD1306 *Disp, OLED_SW *SwState);
+
+// void OLED_Time(Adafruit_SSD1306 *Disp, DateTime *RTCClk);
+// void OLED_Date(Adafruit_SSD1306 *Disp, DateTime *RTCClk);
+// void OLED_Day(Adafruit_SSD1306 *Disp, DateTime *RTCClk);
+
+// void OLED_Range(Adafruit_SSD1306 *Disp, SRFRanges *Rngs);
+// void OLED_Light(Adafruit_SSD1306 *Disp, int LT);
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <Arduino.h>
-#include "WiFi.h"
+//#include <WiFi.h>
 #include "NetWork.h"
+#include "Wire.h"
+//#include "SRF.h"
+#include "OLED.h"
+//#include "settings.h"        // The order is important!
+//#include "sensor_readings.h" // The order is important!
+//#include "network_config.h"
+//#include "SD_Card.h"
+//#include <ezTime.h>
+//#include <TaskScheduler.h>
+
+// assign i2c pin numbers
+#define I2c_SDA 23
+#define I2c_SCL 22
+
+/*******************   oled display   ******************/
+// Declaration for an SSD1306 OLED_Display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 OLED_Display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 /********************************************************   Receiver   *****************************************/
 //ESPNow Feather MAC Adr:         CC:50:E3:BA:CA:A0
@@ -8,19 +25,21 @@
 //ESPNow_Outside Sender #1 ESP32s MAC Adr:  30:AE:A4:46:F0:E4
 // Structure example to receive data
 // Must match the sender structure
-typedef struct struct_message {
-    char a[32];
-    int b;
-    float c;
-    String d;
-    bool e;
+typedef struct struct_message
+{
+  char a[32];
+  int b;
+  float c;
+  String d;
+  bool e;
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes received: ");
   Serial.println(len);
@@ -40,16 +59,17 @@ void setup()
 {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
